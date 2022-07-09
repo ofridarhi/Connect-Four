2966da2a3372163ab8c125e809d4262ac0b9171d
User <ofridarhi12345@gmail.com>
2022-07-09 21:35:49 +0300

    Adding to the Board class the create_board func that adds ampty pieces to the board

--- board.py
+++ board.py
@@ -1,6 +1,6 @@
 import pygame
 import math
-from constants import RED, WIN, BLACK, ROWS, COLS, RADIUS, WIDTH, HEIGHT
+from constants import RED, WIN, BLACK,WHITE, ROWS, COLS, RADIUS, WIDTH, HEIGHT
 
 
 class Board:
@@ -15,4 +15,11 @@ class Board:
         for row in range(padding, HEIGHT + padding, rows_jump):
             for col in range(padding, WIDTH + padding, cols_jump):
                 # surface,color,center,radius,width,draw_top_right,draw_top_left,draw_bottom_left,draw_bottom_right
-                pygame.draw.circle(win, RED, (row, col), RADIUS)
+                pygame.draw.circle(win, WHITE, (row, col), RADIUS)
+
+    def create_board(self):
+        for row in range(ROWS):
+            self.board.append([])
+            for col in range(COLS):
+                self.board[row].append(0)
+
--- constants.py
+++ constants.py
@@ -2,6 +2,8 @@ import pygame
 WIDTH, HEIGHT = 800, 800
 RED = (255,0,0)
 BLACK = (0,0,0)
+WHITE = (255,255,255)
+GREEN = (0,255,255)
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 ROWS, COLS = 7, 6
 RADIUS = 20
