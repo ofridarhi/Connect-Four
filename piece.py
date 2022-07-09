5487aff6b0e67d2c1cda137629f72fcd52a9dbd9
User <ofridarhi12345@gmail.com>
2022-07-09 21:27:03 +0300

    working on class Piece and functions: calc_pos, draw in the class

---
+++ piece.py
@@ -0,0 +1,18 @@
+import pygame
+from constants import RED, GREEN, RADIUS
+class Piece:
+    def __init__(self, row, col, color):
+        self.row = row
+        self.col = col
+        self.color = color
+
+        self.x = 0
+        self.y = 0
+        self.calc_pos()
+
+    def calc_pos(self):
+        self.x = RADIUS * self.col
+        self.y = RADIUS * self.row
+
+    def draw(self,win):
+        pygame.draw.circle(win, self.color, (self.x, self.y), RADIUS)
